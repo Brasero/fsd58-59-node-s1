16b8f14c628e46a30742dcb7baa8dbb227d8151c
Brasero <brandonricci4@gmail.com>
2025-09-01 13:32:34 +0200

    enonce

---
+++ exercice/readStudent.js
@@ -0,0 +1,14 @@
+/*
+* 02 Exercice read students
+  
+  1. Lisez le fichier à l'aide de la méthode asynchrone.
+  1.(bis) Pour la suite, utilisez l'approche synchrone afin de récupérer les données que vous pourrez exploiter par la suite dans le script.
+  
+  2. Recherchez dans le tableau tous les étudiants qui ont eu plus de 17 de moyenne strictement.
+  
+  3. Recherchez dans le tableau l'étudiant qui a eu la meilleur node.
+  
+  4. Récupérez les données dans un objet student, puis ajoutez chaque étudiant dans un tableau students.
+  
+  5. Ordonnez maintenant l'ensemble des données dans le tableau.
+ */
