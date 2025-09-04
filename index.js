6c005e81bbadc463fdb8d0c76f6c6623795e59c5
Brasero <brandonricci4@gmail.com>
2025-09-04 12:10:42 +0200

    correction

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ import pug from "pug";
 
 const cwd = process.cwd()
 const viewPath = path.join(cwd, "view")
-
-const template = `
-if age >= 18
-	h1 Accès autorisé
-else
-	h1 Accès refusé
-`;
-const compilation = pug.compile(template);
+//
+// const template = `
+// if age >= 18
+// 	h1 Accès autorisé
+// else
+// 	h1 Accès refusé
+// `;
+// const compilation = pug.compile(template);
 // console.log(compilation({age: 17}));
 
 // pug.render(template, {age: 17}, (err, data) => {
@@ -36,50 +36,32 @@ const compilation = pug.compile(template);
 // 	console.log(data)
 // })
 
-pug.renderFile(path.join(viewPath, "nav.pug"), {age: 20, name: "toto", html: `<span>Mon html</span>`, pretty: true}, (err, data) => {
+// pug.renderFile(path.join(viewPath, "nav.pug"), {age: 20, name: "toto", html: `<span>Mon html</span>`, pretty: true}, (err, data) => {
+// 	if (err) {
+// 		console.log(err)
+// 		return
+// 	}
+// 	console.log(data)
+// })
+
+const loggedUser = {
+	name: {
+		first: 'Jean',
+		last: 'Dupont',
+	},
+	age: 36,
+	birthdate: new Date('1986-04-18'),
+	location: {
+		zipcode: '77420',
+		city: 'Champs-sur-Marne',
+	},
+	isAdmin: false
+};
+
+pug.renderFile(path.join(viewPath, "user.pug"), {user: loggedUser, pretty: true}, (err, data) => {
 	if (err) {
-		console.log(err)
+		console.error(err)
 		return
 	}
 	console.log(data)
 })
-
-// const server = http.createServer((req, res) => {
-//
-// 	const url = req.url.replace("/", "")
-//
-// 	const header = fs.readFileSync(headerPath, "utf8")
-// 	const footer = fs.readFileSync(footerPath, "utf8")
-//
-// 	if (url === "favicon.ico") {
-// 		res.writeHead(200, {
-// 			"Content-Type": "image/x-icon"
-// 		})
-// 		res.end()
-// 		return
-// 	}
-//
-// 	if (url.startsWith("style")) {
-// 		const filename = url.split("/")[1]
-// 		fs.readFile(path.join(publicPath, filename), "utf8", (err, data) => {
-// 			if(err) {
-// 				res.writeHead(404, {
-// 					"Content-Type": "text/plain"
-// 				})
-// 				res.end(JSON.stringify(err))
-// 			}
-// 			res.writeHead(200, {
-// 				"Content-Type": "text/css"
-// 			})
-// 			res.end(data)
-// 		})
-// 		return
-// 	}
-//
-//
-//
-// })
-//
-// server.listen(port, hostname, () => {
-// 	console.log(`Server listening at http://${hostname}:${port}`)
-// })
